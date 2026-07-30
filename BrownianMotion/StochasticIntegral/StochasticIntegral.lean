af58bd451c3cfed142dd899d29b4c014f450ad93
Joris van Winden <j.van.winden99@gmail.com>
2026-07-30 10:34:25 +0200

    refine characterization

--- BrownianMotion/StochasticIntegral/StochasticIntegral.lean
+++ BrownianMotion/StochasticIntegral/StochasticIntegral.lean
@@ -12,16 +12,15 @@ variable {Ω : Type*} {mΩ : MeasurableSpace Ω} (P : Measure Ω)
 variable {ι : Type*} [LinearOrder ι]
 variable (Y : ι → Ω → ℝ) (𝓕 : Filtration ι mΩ)
 
-/-- A `domain` is a subset of stochastic processes. -/
+/-- A `pre-domain` is a subset of stochastic processes. -/
 abbrev PreDomain := Set (ι → Ω → ℝ)
 /-- A `pre-integral` maps processes to random variables. -/
 abbrev PreIntegral := (X : ι → Ω → ℝ) → Ω → ℝ
 
---- We now write down some properties which any reasonable stochastic integral
---- should satisfy.
-
-/-- A pre-integral `I` with domain `S` respects elementary processes
-if ...
+/--
+A pre-integral `S` respects elementary processes if for any `i ≤ j` and every
+`𝓕 i`-measurable random variable `X`, the elementary process constructed from
+`i`, `j`, and `X` lies in `S` and the value of `I` is the one we would expect.
 -/
 def IntegralElementary (I : PreIntegral) (S : PreDomain) :=
     (α β : ι) → (α ≤ β) → (X : Ω → ℝ) →
@@ -33,23 +32,20 @@ def IntegralElementary (I : PreIntegral) (S : PreDomain) :=
 /-- A pre-integral `I` with domain `S` respects addition if
 `S` is closed under addition and `I` commutes with addition. -/
 def IntegralAdd (I : PreIntegral) (S : PreDomain) :=
-    (X : ι → Ω → ℝ) → (Y : ι → Ω → ℝ) →
-    (X ∈ S) → (Y ∈ S) →
+    (X Y : ι → Ω → ℝ) → (X ∈ S) → (Y ∈ S) →
     X + Y ∈ S ∧ ∀ᵐ ω ∂P, I (X + Y) ω = I X ω + I Y ω
 
 /-- A pre-integral `I` with domain `S` respects scalar multiplication if `S` is
 closed under scalar multiplication and `I` commutes with scalar multiplication. -/
 def IntegralSMul (I : PreIntegral) (S : PreDomain) :=
-    (α : ℝ) → (X : ι → Ω → ℝ) → (X ∈ S) →
+    (X : ι → Ω → ℝ) → (X ∈ S) → (α : ℝ) →
     α • X ∈ S ∧ ∀ᵐ ω ∂P, I (α • X) ω = α * I X ω
 
 /-- A pre-integral `I` with domain `S` respects indistinguishability
 if for every `Y` which is indistinguishable from some `X ∈ S` it holds that
 `Y ∈ S` and `I Y = I X` almost surely. -/
 def IntegralIndistinguishable (I : PreIntegral) (S : PreDomain) :=
-    (X Y : ι → Ω → ℝ) →
-    (h₁ : X ∈ S) →
-    (h₂ : ∀ᵐ ω ∂P, ∀ i, X i ω = X i ω) →
+    (X Y : ι → Ω → ℝ) → (X ∈ S) → (∀ᵐ ω ∂P, ∀ i, X i ω = Y i ω) →
     Y ∈ S ∧ ∀ᵐ ω ∂P, I X ω = I Y ω
 
 /-- A pre-integral `I` with domain `S` respects dominated convergence if, for any
@@ -64,11 +60,11 @@ def IntegralDCT (I : PreIntegral) (S : PreDomain) :=
     (Z ∈ S) ∧ TendstoInMeasure P (fun n ↦ I (X n)) atTop (I Z)
 
 /--
-A pre-integral `I` with domain `S` is a stochastic integral if it
-respects indistinguishability, addition, scalar multiplication, dominated
-converge, and elementary processes.
+A pre-integral `I` with domain `S` is an extension of the Riemann-Stieltjes
+integral if it respects indistinguishability, addition, scalar multiplication,
+dominated converge, and elementary processes.
 -/
-def IsStochasticIntegral (I : PreIntegral) (S : PreDomain) :=
+def IsRiemannStieltjesExtension (I : PreIntegral) (S : PreDomain) :=
     IntegralElementary P Y 𝓕 I S ∧
     IntegralAdd P I S ∧
     IntegralSMul P I S ∧
@@ -76,23 +72,25 @@ def IsStochasticIntegral (I : PreIntegral) (S : PreDomain) :=
     IntegralDCT P I S
 
 /--
-A pre-domain `S` is consistent for `Y` if any two stochastic integrals for `Y`
-with domain `S` must necessarily agree. Using `IntegralDCT` it can be shown that
-this condition holds if every `X ∈ S` can be approximated pointwise by a sequence
-of elementary processes which are dominated by some element of `S`.
+A pre-domain `S` is consistent for `Y` if any there can exists at most one
+extension of the Riemann-Stieltjes integral on `S` (up to a.s. equality).
+This condition ensures that `S` is not accidentally 'too large'.
+It can be shown that `S` is consistent if any function in `S` can be
+approximated pointwise by a sequence of elementary functions in a dominated way.
 -/
 def IsConsistentDomain (S : PreDomain) :=
     (I₁ I₂ : PreIntegral) →
-    IsStochasticIntegral P Y 𝓕 I₁ S →
-    IsStochasticIntegral P Y 𝓕 I₂ S →
+    IsRiemannStieltjesExtension P Y 𝓕 I₁ S →
+    IsRiemannStieltjesExtension P Y 𝓕 I₂ S →
     (X : ι → Ω → ℝ) → (X ∈ S) →
     ∀ᵐ ω ∂P, I₁ X ω = I₂ X ω
 
-def CMSpace (B : NNReal → Ω → ℝ) (hB : IsBrownianReal B P) :=
-    {X : NNReal → Ω → ℝ | IsStronglyPredictable (m := mΩ) (natural B sorry) X}
-
-theorem exists_sintegral (B : NNReal → Ω → ℝ) (𝓕 : Filtration NNReal mΩ)
-    (hB : IsBrownianReal B P) (hB₂ : IsFilteredPreBrownian B 𝓕 P) :
-    ∃ I : PreIntegral, ∃ S : PreDomain,
-    IsStochasticIntegral P B 𝓕 I S ∧ IsConsistentDomain P B 𝓕 S :=
-  sorry
+/-- A `stochastic integral` against `Y` consists of a pre-integral `I`
+and a pre-domain `S`, such that `S` is a consistent domain and `I` is a
+Riemann-Stieltjes extension w.r.t `Y`.
+-/
+structure StochasticIntegral where
+    I : PreIntegral
+    S : PreDomain
+    is_extension : IsRiemannStieltjesExtension P Y 𝓕 I S
+    is_consistent : IsConsistentDomain P Y 𝓕 S
