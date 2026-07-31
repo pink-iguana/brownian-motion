ffbb6cb7419401b01ff42415c31ac89b341a96bd
Etienne Marion <baguettes_casino0c@icloud.com>
2026-07-31 11:26:35 +0200

    done

--- BrownianMotion/StochasticIntegral/PredictableTime.lean
+++ BrownianMotion/StochasticIntegral/PredictableTime.lean
@@ -21,7 +21,7 @@ section Preorder
 
 variable [Preorder ι] {𝓕 : Filtration ι mΩ}
 
-lemma isStoppingTime_piecewise [DecidablePred (· ∈ A)]
+lemma isStoppingTime_piecewise_top [DecidablePred (· ∈ A)]
     (hτ : IsStoppingTime 𝓕 τ) (hA : MeasurableSet[hτ.measurableSpace] A) :
     IsStoppingTime 𝓕 (A.piecewise τ (fun _ ↦ ⊤)) := by
   intro t
@@ -32,13 +32,16 @@ lemma isStoppingTime_piecewise [DecidablePred (· ∈ A)]
 
 variable [OrderBot ι]
 
+/-- The measurable space of the _strict past_ before `τ`. -/
 @[instance_reducible]
 def leftMeasurableSpace (𝓕 : Filtration ι mΩ) (τ : Ω → WithTop ι) : MeasurableSpace Ω :=
   𝓕 ⊥ ⊔ (MeasurableSpace.generateFrom {s | ∃ A t, MeasurableSet[𝓕 t] A ∧ s = A ∩ {ω | t < τ ω}})
 
+/-- A predictable time is a random time whose graph is a predictable set. -/
 def IsPredictableTime (𝓕 : Filtration ι mΩ) (τ : Ω → WithTop ι) : Prop :=
   MeasurableSet[𝓕.predictable] (stochIco τ (fun _ ↦ ⊤))
 
+/-- A predictable time is a stopping time. -/
 lemma IsPredictableTime.isStoppingTime (hτ : IsPredictableTime 𝓕 τ) :
     IsStoppingTime 𝓕 τ := by
   intro t
@@ -55,11 +58,15 @@ lemma IsPredictableTime.isStoppingTime (hτ : IsPredictableTime 𝓕 τ) :
     · exact (𝓕.mono h.le) A hA
     · convert MeasurableSet.empty
 
+/-- An accessible time is a stopping time whose graph is contained in the union of the graphs
+a sequence of predictable times. -/
 structure IsAccessibleTime (𝓕 : Filtration ι mΩ) (τ : Ω → WithTop ι) : Prop where
   isStoppingTime : IsStoppingTime 𝓕 τ
   stochIco_subset : ∃ σ : ℕ → Ω → WithTop ι,
     (∀ n, IsPredictableTime 𝓕 (σ n)) ∧ stochGraph τ ⊆ ⋃ n, stochGraph (σ n)
 
+/-- An inaccessible time is a stopping time `τ` such that for any predictable time `σ`,
+`P(σ = τ < ⊤) = 0`. -/
 structure IsInacessibleTime (𝓕 : Filtration ι mΩ) (τ : Ω → WithTop ι) (P : Measure Ω) : Prop where
   isStoppingTime : IsStoppingTime 𝓕 τ
   measure_eq_lt_top_eq_zero : ∀ σ, IsPredictableTime 𝓕 σ → P {ω | τ ω = σ ω ∧ σ ω < ⊤} = 0
@@ -68,52 +75,13 @@ end Preorder
 
 variable [LinearOrder ι] [OrderBot ι] {𝓕 : Filtration ι mΩ}
 
-lemma test [TopologicalSpace ι] [OrderTopology ι] [SecondCountableTopology ι]
-    (hσ : IsStoppingTime 𝓕 σ) (hA : MeasurableSet[hσ.measurableSpace] A) :
-    MeasurableSet[leftMeasurableSpace 𝓕 τ] (A ∩ {ω | σ ω < τ ω}) := by
-  obtain ⟨s, cs, ds⟩ := TopologicalSpace.exists_countable_dense ι
-  let t := s ∪ {x | 𝓝[>] x = ⊥}
-  have ct : t.Countable := cs.union countable_setOfPred_isolated_right
-  have dt : Dense t := ds.mono (by grind)
-  have : A ∩ {ω | σ ω < τ ω} = ⋃ x ∈ t, A ∩ {ω | σ ω ≤ x} ∩ {ω | x < τ ω} := by
-    ext ω
-    simp only [Set.mem_inter_iff, Set.mem_ofPred_eq, Set.mem_iUnion, exists_and_left,
-      exists_prop]
-    refine ⟨fun ⟨h1, h2⟩ ↦ ?_, fun ⟨i, ⟨hi1, hi2⟩, hi3, hi4⟩ ↦ ⟨hi1, hi2.trans_lt hi4⟩⟩
-    cases h' : σ ω with
-    | top => simp_all
-    | coe i =>
-      obtain h | h := (𝓝[>] i).eq_or_neBot
-      · exact ⟨i, ⟨h1, le_rfl⟩, by grind, by rwa [← h']⟩
-      · cases h'' : τ ω with
-        | top =>
-          have : (Set.Ioi i).Nonempty := nonempty_of_mem (f := 𝓝[>] i)
-            self_mem_nhdsWithin
-          obtain ⟨j, hj1, hj2⟩ := dt.exists_mem_open isOpen_Ioi this
-          refine ⟨j, ⟨h1, ?_⟩, hj1, by simp⟩
-          simpa using hj2.le
-        | coe j =>
-          have : (Set.Ioo i j).Nonempty := by
-            convert nonempty_of_mem (f := 𝓝[>] i)
-              (inter_mem_nhdsWithin _ (t := Set.Iio j) ?_)
-            · rw [Set.Ioi_inter_Iio]
-            · apply Iio_mem_nhds
-              rwa [← WithTop.coe_lt_coe, ← h', ← h'']
-          obtain ⟨k, hk1, hk2⟩ := dt.exists_mem_open isOpen_Ioo this
-          refine ⟨k, ⟨h1, by simpa using hk2.1.le⟩, hk1, by simpa using hk2.2⟩
-  rw [this]
-  refine .biUnion ct fun x _ ↦ MeasurableSpace.measurableSet_generateFrom ?_
-  simp only [Set.sup_eq_union, Set.mem_union, Set.mem_ofPred_eq]
-  right
-  apply MeasurableSpace.measurableSet_generateFrom
-  exact ⟨A ∩ {ω | σ ω ≤ x}, x, ((hσ.measurableSet A).1 hA).2 x, rfl⟩
-
 theorem Set.iInter_prod {α β ι : Type*} {s : Set α} {t : ι → Set β} [hι : Nonempty ι] :
     (⋂ (i : ι), t i) ×ˢ s = ⋂ (i : ι), t i ×ˢ s := by
   ext
   simp only [Set.mem_prod, Set.mem_iInter]
   exact ⟨fun h i ↦ ⟨h.1 i, h.2⟩, fun h ↦ ⟨fun i ↦ (h i).1, (h Classical.ofNonempty).2⟩⟩
 
+/-- A constant time is predictable. -/
 lemma IsPredictableTime.const [TopologicalSpace ι] [OrderTopology ι] [FirstCountableTopology ι]
     (𝓕 : Filtration ι mΩ) (t : WithTop ι) :
     IsPredictableTime 𝓕 (fun _ ↦ t) := by
@@ -187,8 +155,8 @@ theorem IsStoppingTime.decompositon
       hτ.measurableSet_lt_top
   let B := ⋃ n, ⋃ k, A n k
   have mB : MeasurableSet[hτ.measurableSpace] B := .iUnion fun n ↦ .iUnion fun k ↦ (mA n k)
-  refine ⟨B, mB, by simp +contextual [A, B], ⟨isStoppingTime_piecewise hτ mB, ?_⟩,
-    ⟨isStoppingTime_piecewise hτ mB.compl, fun π hπ ↦ ?_⟩⟩
+  refine ⟨B, mB, by simp +contextual [A, B], ⟨isStoppingTime_piecewise_top hτ mB, ?_⟩,
+    ⟨isStoppingTime_piecewise_top hτ mB.compl, fun π hπ ↦ ?_⟩⟩
   · obtain ⟨φ, hφ⟩ := exists_surjective_nat (ℕ × ℕ)
     refine ⟨fun n ↦ σ (φ n).1 (φ n).2, fun n ↦ hσ1 _ _, fun tω h ↦ ?_⟩
     simp only [stochGraph, Set.piecewise, Set.mem_iUnion, Set.mem_ofPred_eq, B, A] at h ⊢
@@ -262,44 +230,5 @@ theorem IsStoppingTime.decompositon
           rintro - ⟨_, _, rfl⟩
           exact measure_mono (by simp)
     _ ≤ sSup S := final
-
-def IsThinSet (s : Set (ι × Ω)) (𝓕 : Filtration ι mΩ) : Prop :=
-  ∃ τ : ℕ → Ω → WithTop ι, (∀ n, IsStoppingTime 𝓕 (τ n)) ∧ s = ⋃ n, stochGraph (τ n)
-
-lemma IsThinSet.mono {s t : Set (ι × Ω)} (ht : IsThinSet t 𝓕) (hst : s ⊆ t)
-    [MeasurableSpace ι] [TopologicalSpace ι] [OrderTopology ι] [SecondCountableTopology ι]
-    [BorelSpace ι]
-    (hs' : IsStronglyProgressive 𝓕 (fun t ω ↦ s.indicator (1 : ι × Ω → ℝ) (t, ω))) :
-    IsThinSet s 𝓕 := by
-  obtain ⟨τ, hτ, rfl⟩ := ht
-  let L n : Set Ω := {ω | ∃ (h : τ n ω ≠ ⊤), ((τ n ω).untop h, ω) ∈ s}
-  classical
-  refine ⟨fun n ↦ (L n).piecewise (τ n) (fun _ ↦ ⊤), fun n ↦ isStoppingTime_piecewise (hτ n) ?_, ?_⟩
-  · rw [← measurable_indicator_const_iff (b := (1 : ℝ))]
-    have : (L n).indicator (fun _ ↦ (1 : ℝ)) =
-        {ω | τ n ω ≠ ⊤}.indicator
-          (stoppedValue (fun u ω ↦ s.indicator (1 : ι × Ω → ℝ) (u, ω)) (τ n)) := by
-      ext ω
-      cases h : τ n ω with
-      | top => simp [L, h]
-      | coe u => simp [h, Set.indicator, L, stoppedValue]
-    rw [this]
-    refine .indicator ?_ ((measurableSet_singleton ⊤).preimage (hτ n).measurable).compl
-    exact measurable_stoppedValue hs' (hτ n)
-  · ext uω
-    simp only [stochGraph, Set.piecewise, ne_eq, Set.mem_ofPred_eq, Set.mem_iUnion, L]
-    refine ⟨fun h ↦ ?_, fun ⟨n, hn⟩ ↦ ?_⟩
-    · have := hst h
-      simp only [stochGraph, Set.mem_iUnion, Set.mem_ofPred_eq] at this
-      obtain ⟨n, hn⟩ := this
-      refine ⟨n, ?_⟩
-      rw [if_pos, hn]
-      rw [← hn]
-      simpa
-    split_ifs at hn with h
-    · obtain ⟨h1, h2⟩ := h
-      rw! [← hn, WithTop.untop_coe] at h2
-      exact h2
-    · contradiction
 
 end MeasureTheory
